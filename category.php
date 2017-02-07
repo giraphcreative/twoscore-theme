4731071f9188ec86af8bdccf07d78b835df5edb2
James Pederson <james@jpederson.com>
2017-02-07 00:32:58 -0600

    Add category template and bump stylesheet.

---
+++ category.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Home/catch-all template
+*/
+
+get_header();
+
+?>
+
+	<section role="main">
+		
+		<header>
+			<h2>Blog</h2>
+		</header>
+		<section class="main-content blog-listing">
+			<h2>Posts in <?php single_cat_title() ?></h2>
+
+		<?php
+		while ( have_posts() ) : the_post();
+			?>
+			<article>
+				<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+				<?php the_excerpt(); ?>
+				<p class="meta">Posted by <?php the_author_link() ?> in  <span>on <?php the_date() ?></span></p>
+			</article>
+			<?php
+		endwhile;
+		?>
+
+			<div class="pagination">
+				<?php pagination(); ?>
+			</div>
+		</section>
+
+	</section><!-- #primary -->
+
+
+<?php get_footer(); ?>
